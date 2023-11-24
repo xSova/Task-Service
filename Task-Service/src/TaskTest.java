456fca8313060a3d9387153ee840621eaf8ae230
xSova <25107568+xSova@users.noreply.github.com>
2023-11-24 01:50:58 -0600

    Small changes

--- Task-Service/src/TaskTest.java
+++ Task-Service/src/TaskTest.java
@@ -1,6 +1,6 @@
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 public class TaskTest {
 
